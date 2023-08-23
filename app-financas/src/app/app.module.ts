9c241ecea40cc5ef6a7072c650c5a78d01366db7
Roberto Murase <robertomurase@gmail.com>
2023-08-23 12:18:36 -0300

    Criacao da tela de Login

--- app-financas/src/app/app.module.ts
+++ app-financas/src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AuthService } from './auth.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent, LoginComponent, HeaderComponent],
   imports: [
+    CommonModule,
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyBi3tglDjnsRYdx4u_HzcrXiSTjJM-sMFo',
+      authDomain: 'app-financas-39000.firebaseapp.com',
+      databaseURL: 'https://app-financas-39000-default-rtdb.firebaseio.com',
+      projectId: 'app-financas-39000',
+      storageBucket: 'app-financas-39000.appspot.com',
+      messagingSenderId: '529808553682',
+      appId: '1:529808553682:web:450a1183af820c707c5f0b',
+    }),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AuthService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ app-financas/src/app/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth.service';
+import { map } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import {
+  AngularFireDatabase,
+  SnapshotAction,
+} from '@angular/fire/compat/database';
+
+export interface Item {
+  key: string;
+  email: string;
+  name: string;
+  age: string;
+}
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  email = '' as string;
+  password = '' as string;
+
+  formNewName = '' as string;
+  formNewAge = '' as string;
+
+  listRef: any;
+  list: Observable<Item[]>;
+
+  constructor(public auth: AuthService, private database: AngularFireDatabase) {
+    this.listRef = database.list('list');
+    this.list = this.listRef
+      .snapshotChanges()
+      .pipe(
+        map((changes: SnapshotAction<Item>[]) =>
+          changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))
+        )
+      );
+  }
+
+  ngOnInit() {}
+
+  addItem() {
+    this.listRef.push({
+      name: this.formNewName,
+      age: this.formNewAge,
+      email: this.auth.user.email,
+    });
+    this.formNewName = '';
+    this.formNewAge = '';
+  }
+
+  deleteItem(key: string) {
+    this.listRef.remove(key);
+  }
+}
